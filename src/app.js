e30328a3b5209d9102c6151ecc8e3b7eeb495440
Blkthor23 <JayDaKing23@gmail.com>
2022-02-04 09:38:36 -0600

    heroku deploy

--- src/app.js
+++ src/app.js
@@ -1,25 +1,29 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
-const moviesRouter = require("./movies/movies.router")
-const reviewsRouter = require("./reviews/reviews.router")
-const theaterRouter = require("./theaters/theaters.router")
-const app = express()
+const app = express();
+const cors = require("cors");
+const moviesRouter = require("./movies/movies.router");
+const reviewsRouter = require("./reviews/reviews.router");
+const theaterRouter = require("./theaters/theaters.router");
 
-app.use(express.json())
+//json and cors enabled
+app.use(cors());
+app.use(express.json());
 
-app.use("/movies", moviesRouter)
-app.use("/reviews", reviewsRouter)
-app.use("/theaters", theaterRouter)
+//routes
+app.use("/movies", moviesRouter);
+app.use("/reviews", reviewsRouter);
+app.use("/theaters", theaterRouter);
 
 //not found handler
 app.use((req, res, next) => {
-    next({ status: 404, message: `Not found: ${req.originalUrl}` })
-  })
+  next({ status: 404, message: `Not found: ${req.originalUrl}` });
+});
 
-  //error handler
+//error handler
 app.use((error, req, res, next) => {
-    const { status = 500, message = "Something went wrong!" } = error
-    res.status(status).json({ error: message })
-})
+  const { status = 500, message = "Something went wrong!" } = error;
+  res.status(status).json({ error: message });
+});
 
 module.exports = app;
